cbb82372521ade07e6bba4704feb271371dc8e2b
Irvan Rifa'i <ivan.rivai6921@gmail.com>
2025-06-11 10:26:40 +0700

    add env example and implement env in start dev

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
+import { configDotenv } from "dotenv";
+configDotenv();
 
-web.listen(3000, () => {
-  logger.info("App start on port 3000");
+web.listen(process.env.APP_PORT, () => {
+  logger.info(`App start on port ${process.env.APP_PORT}`);
 })

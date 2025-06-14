c1ed22fa5d91bc1fd3bf2ec2134115be4c955562
Irvan Rifa'i <ivan.rivai6921@gmail.com>
2025-06-14 20:20:17 +0700

    add health check api

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -3,6 +3,11 @@ import userController from "../controller/user-controller.js";
 
 const publicRouter = new express.Router();
 
+// Health check route
+publicRouter.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
 // public route
 publicRouter.post("/api/users", userController.register);
 publicRouter.post("/api/users/login", userController.login);

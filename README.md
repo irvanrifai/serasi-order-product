7671fd32047cd020f1997047a88200fff0ab070e
Irvan Rifa'i <ivan.rivai6921@gmail.com>
2026-06-04 09:08:20 +0700

    wip format schema migration

---
+++ README.md
@@ -0,0 +1,10 @@
+# installation
+cp .env.example .env
+npm install
+
+# migration database using prisma
+
+# running app
+node src/main.js
+
+# test app
--- prisma/migrations/20250531072304_create_users_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `users` (
-    `username` CHAR(100) NOT NULL,
-    `password` CHAR(100) NOT NULL,
-    `name` CHAR(100) NOT NULL,
-    `token` CHAR(100) NULL,
-
-    PRIMARY KEY (`username`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE innoDB;
